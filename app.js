bf5ecf3fcbd1db97f5c408f919bd2045d7add574
Ryan Manro <rmanro@gmail.com>
2018-02-05 14:57:46 -0800

    added first list structure for first location in html

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 'use strict';
 
 const pdxAirport = {
-    name: 'PDX Airport',
+    id: 'pdxairport',
     min: 23,
     max: 65,
     avg: 6.3,
     hourSales: [],
     custPerHour: function(){
-        return Math.floor(Math.random () * (this.max - this.min) + this.min);
+        return Math.floor(Math.random () * (this.max - this.min) + this.min); //random # of customers
     },
     cookiesPerHour: function(){
-        return Math.floor(this.avg * this.custPerHour());
+        return Math.floor(this.avg * this.custPerHour()); //avg cookies per hour
     },
-    cookiesEachHour: function(){
+    cookiesEachHour: function(){    //avg cookies for each hour of workday - 15 total
         for (let i = 0; i < 15; i++){
             this.hourSales[i] = this.cookiesPerHour();
         }
