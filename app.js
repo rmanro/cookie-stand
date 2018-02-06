79d5b4f50faa02da3e3611b6471d592ee44f81b0
Ryan Manro <rmanro@gmail.com>
2018-02-06 13:15:26 -0800

    created constructor for locations and tested 1st one

--- app.js
+++ app.js
@@ -30,6 +30,37 @@ const pdxAirport = {
     }
 };
 
+function Location (id, min, max, avg){
+    this.id = id;
+    this.min = min;
+    this.max = max;
+    this.avg = avg;
+    this.hourSales = [];
+    this.hoursOfDay = ['6AM:','7AM:','8AM:','9AM:','10AM:','11AM:','12PM:','1PM:','2PM:','3PM:','4PM:','5PM:','6PM:','7PM:','8PM:'];
+    this.totalSales = 0;
+}
+
+Location.prototype.renderLocation = function(){
+    for (let i = 0; i < 15; i++){            //avg cookies for each hour of workday - 15 total
+        const custPerHour = Math.floor(Math.random () * (this.max - this.min) + this.min);    //random # of customers
+        const cookiesPerHour = Math.floor(this.avg * custPerHour);      //avg cookies per hour
+        this.hourSales[i] = cookiesPerHour;
+        this.totalSales += this.hourSales[i];
+    }
+    for (let i = 0; i < this.hoursOfDay.length; i++)
+    {
+        const list = document.getElementById(this.id);
+        const li = document.createElement('li');
+        li.textContent = `${this.hoursOfDay[i]} ${this.hourSales[i]}`;
+        console.log(li.textContent);
+        list.appendChild(li);
+    }
+    const total = document.getElementById(this.id);
+    const totalli = document.createElement('li');
+    totalli.textContent = `Total: ${this.totalSales}`;
+    total.appendChild(totalli);
+};
+
 const pioneerSquare = {
     id: 'pioneersquare',
     min: 3,
@@ -178,7 +209,10 @@ const waterfront = {
     }
 };
 
-pdxAirport.renderLocation();
+const location1 = new Location('pdxairport', 23, 65, 6.3);
+location1.renderLocation();
+
+// pdxAirport.renderLocation();
 pioneerSquare.renderLocation();
 powells.renderLocation();
 stJohns.renderLocation();
