bb2eb542efaca71ab6198833c8db8ecf4351f8b9
Ryan Manro <rmanro@gmail.com>
2018-02-06 12:56:16 -0800

    merged cookiesperHour method into cookiesEachHour method

--- app.js
+++ app.js
@@ -8,13 +8,11 @@ const pdxAirport = {
     hourSales: [],
     hoursOfDay: ['6AM:','7AM:','8AM:','9AM:','10AM:','11AM:','12PM:','1PM:','2PM:','3PM:','4PM:','5PM:','6PM:','7PM:','8PM:'],
     totalSales: 0,
-    cookiesPerHour: function(){
-        const custPerHour = Math.floor(Math.random () * (this.max - this.min) + this.min); //random # of customers
-        return Math.floor(this.avg * custPerHour); //avg cookies per hour
-    },
     cookiesEachHour: function(){    //avg cookies for each hour of workday - 15 total
         for (let i = 0; i < 15; i++){
-            this.hourSales[i] = this.cookiesPerHour();
+            const custPerHour = Math.floor(Math.random () * (this.max - this.min) + this.min); //random # of customers
+            const cookiesPerHour = Math.floor(this.avg * custPerHour); //avg cookies per hour
+            this.hourSales[i] = cookiesPerHour;
             this.totalSales += this.hourSales[i];
         }
     },
