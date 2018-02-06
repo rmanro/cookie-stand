de4e04248a6de0227ce1e2b6c1a9be83628aa458
Ryan Manro <rmanro@gmail.com>
2018-02-06 13:24:33 -0800

    all locations using the constructor now

--- app.js
+++ app.js
@@ -1,35 +1,5 @@
 'use strict';
 
-const pdxAirport = {
-    id: 'pdxairport',
-    min: 23,
-    max: 65,
-    avg: 6.3,
-    hourSales: [],
-    hoursOfDay: ['6AM:','7AM:','8AM:','9AM:','10AM:','11AM:','12PM:','1PM:','2PM:','3PM:','4PM:','5PM:','6PM:','7PM:','8PM:'],
-    totalSales: 0,
-    renderLocation: function(){
-        for (let i = 0; i < 15; i++){            //avg cookies for each hour of workday - 15 total
-            const custPerHour = Math.floor(Math.random () * (this.max - this.min) + this.min);    //random # of customers
-            const cookiesPerHour = Math.floor(this.avg * custPerHour);      //avg cookies per hour
-            this.hourSales[i] = cookiesPerHour;
-            this.totalSales += this.hourSales[i];
-        }
-        for (let i = 0; i < this.hoursOfDay.length; i++)
-        {
-            const list = document.getElementById(this.id);
-            const li = document.createElement('li');
-            li.textContent = `${this.hoursOfDay[i]} ${this.hourSales[i]}`;
-            console.log(li.textContent);
-            list.appendChild(li);
-        }
-        const total = document.getElementById(this.id);
-        const totalli = document.createElement('li');
-        totalli.textContent = `Total: ${this.totalSales}`;
-        total.appendChild(totalli);
-    }
-};
-
 function Location (id, min, max, avg){
     this.id = id;
     this.min = min;
@@ -61,160 +31,18 @@ Location.prototype.renderLocation = function(){
     total.appendChild(totalli);
 };
 
-const pioneerSquare = {
-    id: 'pioneersquare',
-    min: 3,
-    max: 24,
-    avg: 1.2,
-    hourSales: [],
-    hoursOfDay: ['6AM:','7AM:','8AM:','9AM:','10AM:','11AM:','12PM:','1PM:','2PM:','3PM:','4PM:','5PM:','6PM:','7PM:','8PM:'],
-    totalSales: 0,
-    custPerHour: function(){
-        return Math.floor(Math.random () * (this.max - this.min) + this.min); //random # of customers
-    },
-    cookiesPerHour: function(){
-        return Math.floor(this.avg * this.custPerHour()); //avg cookies per hour
-    },
-    cookiesEachHour: function(){    //avg cookies for each hour of workday - 15 total
-        for (let i = 0; i < 15; i++){
-            this.hourSales[i] = this.cookiesPerHour();
-            this.totalSales += this.hourSales[i];
-        }
-    },
-    renderLocation: function(){
-        this.cookiesEachHour();
-        for (let i = 0; i < this.hoursOfDay.length; i++)
-        {
-            const list = document.getElementById(this.id);
-            const li = document.createElement('li');
-            li.textContent = `${this.hoursOfDay[i]} ${this.hourSales[i]}`;
-            console.log(li.textContent);
-            list.appendChild(li);
-        }
-        const total = document.getElementById(this.id);
-        const totalli = document.createElement('li');
-        totalli.textContent = `Total: ${this.totalSales}`;
-        total.appendChild(totalli);
-    }
-};
-
-const powells = {
-    id: 'powells',
-    min: 11,
-    max: 38,
-    avg: 3.7,
-    hourSales: [],
-    hoursOfDay: ['6AM:','7AM:','8AM:','9AM:','10AM:','11AM:','12PM:','1PM:','2PM:','3PM:','4PM:','5PM:','6PM:','7PM:','8PM:'],
-    totalSales: 0,
-    custPerHour: function(){
-        return Math.floor(Math.random () * (this.max - this.min) + this.min); //random # of customers
-    },
-    cookiesPerHour: function(){
-        return Math.floor(this.avg * this.custPerHour()); //avg cookies per hour
-    },
-    cookiesEachHour: function(){    //avg cookies for each hour of workday - 15 total
-        for (let i = 0; i < 15; i++){
-            this.hourSales[i] = this.cookiesPerHour();
-            this.totalSales += this.hourSales[i];
-        }
-    },
-    renderLocation: function(){
-        this.cookiesEachHour();
-        for (let i = 0; i < this.hoursOfDay.length; i++)
-        {
-            const list = document.getElementById(this.id);
-            const li = document.createElement('li');
-            li.textContent = `${this.hoursOfDay[i]} ${this.hourSales[i]}`;
-            console.log(li.textContent);
-            list.appendChild(li);
-        }
-        const total = document.getElementById(this.id);
-        const totalli = document.createElement('li');
-        totalli.textContent = `Total: ${this.totalSales}`;
-        total.appendChild(totalli);
-    }
-};
-
-const stJohns = {
-    id: 'stjohns',
-    min: 20,
-    max: 38,
-    avg: 2.3,
-    hourSales: [],
-    hoursOfDay: ['6AM:','7AM:','8AM:','9AM:','10AM:','11AM:','12PM:','1PM:','2PM:','3PM:','4PM:','5PM:','6PM:','7PM:','8PM:'],
-    totalSales: 0,
-    custPerHour: function(){
-        return Math.floor(Math.random () * (this.max - this.min) + this.min); //random # of customers
-    },
-    cookiesPerHour: function(){
-        return Math.floor(this.avg * this.custPerHour()); //avg cookies per hour
-    },
-    cookiesEachHour: function(){    //avg cookies for each hour of workday - 15 total
-        for (let i = 0; i < 15; i++){
-            this.hourSales[i] = this.cookiesPerHour();
-            this.totalSales += this.hourSales[i];
-        }
-    },
-    renderLocation: function(){
-        this.cookiesEachHour();
-        for (let i = 0; i < this.hoursOfDay.length; i++)
-        {
-            const list = document.getElementById(this.id);
-            const li = document.createElement('li');
-            li.textContent = `${this.hoursOfDay[i]} ${this.hourSales[i]}`;
-            console.log(li.textContent);
-            list.appendChild(li);
-        }
-        const total = document.getElementById(this.id);
-        const totalli = document.createElement('li');
-        totalli.textContent = `Total: ${this.totalSales}`;
-        total.appendChild(totalli);
-    }
-};
-
-const waterfront = {
-    id: 'waterfront',
-    min: 2,
-    max: 16,
-    avg: 4.6,
-    hourSales: [],
-    hoursOfDay: ['6AM:','7AM:','8AM:','9AM:','10AM:','11AM:','12PM:','1PM:','2PM:','3PM:','4PM:','5PM:','6PM:','7PM:','8PM:'],
-    totalSales: 0,
-    custPerHour: function(){
-        return Math.floor(Math.random () * (this.max - this.min) + this.min); //random # of customers
-    },
-    cookiesPerHour: function(){
-        return Math.floor(this.avg * this.custPerHour()); //avg cookies per hour
-    },
-    cookiesEachHour: function(){    //avg cookies for each hour of workday - 15 total
-        for (let i = 0; i < 15; i++){
-            this.hourSales[i] = this.cookiesPerHour();
-            this.totalSales += this.hourSales[i];
-        }
-    },
-    renderLocation: function(){
-        this.cookiesEachHour();
-        for (let i = 0; i < this.hoursOfDay.length; i++)
-        {
-            const list = document.getElementById(this.id);
-            const li = document.createElement('li');
-            li.textContent = `${this.hoursOfDay[i]} ${this.hourSales[i]}`;
-            console.log(li.textContent);
-            list.appendChild(li);
-        }
-        const total = document.getElementById(this.id);
-        const totalli = document.createElement('li');
-        totalli.textContent = `Total: ${this.totalSales}`;
-        total.appendChild(totalli);
-    }
-};
-
 const location1 = new Location('pdxairport', 23, 65, 6.3);
 location1.renderLocation();
 
-// pdxAirport.renderLocation();
-pioneerSquare.renderLocation();
-powells.renderLocation();
-stJohns.renderLocation();
-waterfront.renderLocation();
+const location2 = new Location('pioneersquare', 3, 24, 1.2);
+location2.renderLocation();
+
+const location3 = new Location('powells', 11, 38, 3.7);
+location3.renderLocation();
+
+const location4 = new Location('stjohns', 20, 38, 2.3);
+location4.renderLocation();
+
+const location5 = new Location('waterfront', 2, 16, 4.6);
+location5.renderLocation();
 
