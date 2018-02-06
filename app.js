d3779aebc482c3065ddb2db5c6c5b8040677e816
Ryan Manro <rmanro@gmail.com>
2018-02-06 15:00:52 -0800

    added build row function

--- app.js
+++ app.js
@@ -3,11 +3,12 @@
 const hoursOfDay = ['6AM:','7AM:','8AM:','9AM:','10AM:','11AM:','12PM:','1PM:','2PM:','3PM:','4PM:','5PM:','6PM:','7PM:','8PM:'];
 
 
-function Location (id, min, max, avg){
+function Location (id, min, max, avg, locationName){
     this.id = id;
     this.min = min;
     this.max = max;
     this.avg = avg;
+    this.locationName = locationName;
     this.hourSales = [];
     this.totalSales = 0;
 }
@@ -19,22 +20,21 @@ Location.prototype.renderLocation = function(){
         this.hourSales[i] = cookiesPerHour;
         this.totalSales += this.hourSales[i];
     }
-    for (let i = 0; i < this.hoursOfDay.length; i++)
-    {
-        const list = document.getElementById(this.id);
-        const li = document.createElement('li');
-        li.textContent = `${this.hoursOfDay[i]} ${this.hourSales[i]}`;
-        console.log(li.textContent);
-        list.appendChild(li);
-    }
-    const total = document.getElementById(this.id);
-    const totalli = document.createElement('li');
-    totalli.textContent = `Total: ${this.totalSales}`;
-    total.appendChild(totalli);
+    // for (let i = 0; i < this.hoursOfDay.length; i++)
+    // {
+    //     const list = document.getElementById(this.id);
+    //     const li = document.createElement('li');
+    //     li.textContent = `${this.hoursOfDay[i]} ${this.hourSales[i]}`;
+    //     console.log(li.textContent);
+    //     list.appendChild(li);
+    // }
+    // const total = document.getElementById(this.id);
+    // const totalli = document.createElement('li');
+    // totalli.textContent = `Total: ${this.totalSales}`;
+    // total.appendChild(totalli);
 };
 
 const buildHourHeaders = function(){
-    alert('test');
     const theader = document.querySelector('#sales thead');
     const tr = document.createElement('tr');
     for (let i = 0; i < 15; i++){
@@ -49,20 +49,35 @@ const buildHourHeaders = function(){
     theader.appendChild(tr);
 };
 
+Location.prototype.buildLocationRow = function(){
+    const tbody = document.querySelector('#sales tbody');
+    const tr = document.createElement('tr');
+    const loctd = document.createElement('td');
+    loctd.textContent = this.locationName;
+    tr.appendChild(loctd);
+    for (let i = 0; i < 15; i++){
+        const td = document.createElement('td');
+        td.textContent = this.hourSales[i];
+        tr.appendChild(td);
+    }
+    tbody.appendChild(tr);
+};
+
 buildHourHeaders();
 
-// const location1 = new Location('pdxairport', 23, 65, 6.3);
-// location1.renderLocation();
+const location1 = new Location('pdxairport', 23, 65, 6.3, 'PDX Airport');
+location1.renderLocation();
+location1.buildLocationRow();
 
-// const location2 = new Location('pioneersquare', 3, 24, 1.2);
+// const location2 = new Location('pioneersquare', 3, 24, 1.2, 'Pioneer Square');
 // location2.renderLocation();
 
-// const location3 = new Location('powells', 11, 38, 3.7);
+// const location3 = new Location('powells', 11, 38, 3.7, 'Powell\'s');
 // location3.renderLocation();
 
-// const location4 = new Location('stjohns', 20, 38, 2.3);
+// const location4 = new Location('stjohns', 20, 38, 2.3, 'St John\'s');
 // location4.renderLocation();
 
-// const location5 = new Location('waterfront', 2, 16, 4.6);
+// const location5 = new Location('waterfront', 2, 16, 4.6, 'Waterfront');
 // location5.renderLocation();
 
