a24f5328818298f04fe0ac9e90aa58e0e88f79e6
Ryan Manro <rmanro@gmail.com>
2018-02-06 16:27:22 -0800

    all store totals combined for city total showing in table

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 const hoursOfDay = ['6AM:','7AM:','8AM:','9AM:','10AM:','11AM:','12PM:','1PM:','2PM:','3PM:','4PM:','5PM:','6PM:','7PM:','8PM:'];
 let totalHourSales = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+let totalAllSales = 0;
 
 function Location (id, min, max, avg, locationName){
     this.id = id;
@@ -19,6 +20,7 @@ Location.prototype.renderLocation = function(){
         const cookiesPerHour = Math.floor(this.avg * custPerHour);      //avg cookies per hour
         this.hourSales[i] = cookiesPerHour;
         this.totalSales += this.hourSales[i];
+        totalAllSales += this.totalSales;
     }
     // for (let i = 0; i < this.hoursOfDay.length; i++)
     // {
@@ -83,9 +85,9 @@ const buildFooter = function(){
         tr.appendChild(td);
     }
     tfoot.appendChild(tr);
-    // const totaltd = document.createElement('td');
-    // totaltd.textContent = this.totalSales;
-    // tr.appendChild(totaltd);
+    const totalfoot = document.createElement('td');
+    totalfoot.textContent = totalAllSales;
+    tr.appendChild(totalfoot);
 };
 
 buildHourHeaders();
